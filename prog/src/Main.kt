113b6c2862b947cb1ca654539338fedc4bb5b21b
Hamjoshua <biryukov-10@list.ru>
2024-09-09 23:03:14 +0700

    Update Main.kt

--- prog/src/Main.kt
+++ prog/src/Main.kt
@@ -1,14 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+}
+
+open class Vehicle(var name: String = "Транспорт", var velocity: Int = 0) {
+    open fun start() {
+        println("$name начал движение ($velocity км/ч)")
+    }
+
+    // Метод для остановки
+    open fun stop() {
+        println("$name остановился")
+    }
+}
+
+class Boat(name: String = "Лодка", velocity: Int = 4) : Vehicle(name, velocity){
+    override fun start(){
+        println("$name начал плыть ($velocity км/ч)")
+    }
+
+    override fun stop() {
+        println("$name сбросила якорь ($velocity км/ч)")
+    }
+}
+
+class Plane(name: String = "Самолет", velocity: Int = 100) : Vehicle(name, velocity){
+    override fun start(){
+        println("$name взлетел ($velocity км/ч)")
+    }
+
+    override fun stop() {
+        println("$name сел на посадку ($velocity км/ч)")
+    }
+}
+
+class Tank(name: String = "Танк", velocity: Int = 50) : Vehicle(name, velocity){
+    override fun start(){
+        println("$name начал выдвигаться на позицию ($velocity км/ч)")
+    }
+
+    override fun stop() {
+        println("$name занял позицию ($velocity км/ч)")
     }
 }
