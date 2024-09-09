b322d61c4a0c87cc3ad58c8773d58ad0d7fc8780
Hamjoshua <biryukov-10@list.ru>
2024-09-09 20:21:26 +0700

    Update Main.kt

--- prog/src/Main.kt
+++ prog/src/Main.kt
@@ -1,14 +1,42 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    // 1 task
+    val number:String = readln()
+    val sum:Int = number.first().digitToInt() + number.last().digitToInt()
+    println("Сумма первой и второй цифры введенного числа $number равна $sum")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    // 2 task
+    var inputLine:String = readln()
+    var nums: IntArray = intArrayOf()
+
+    while (inputLine != "0"){
+        inputLine = readln()
+        nums += inputLine.toInt()
+    }
+
+    println("Количество: ${nums.count()}\nСумма: ${nums.sum()}\nСр. знач.: ${nums.average()}")
+
+    // 3 task
+    var rightNumber:Int =  (0..10).random()
+
+    println("Загадано число от 0 до 10. Попробуйте угадать!")
+
+    while (true) {
+        print("Введите ваше число: ")
+        val guess = readLine()?.toIntOrNull()
+
+        if (guess == null) {
+            println("Некорректный ввод. Пожалуйста, введите целое число.")
+        } else {
+            if (guess > rightNumber) {
+                println("Много!")
+            } else if (guess < rightNumber) {
+                println("Мало!")
+            } else {
+                println("Угадал!")
+                break
+            }
+        }
     }
 }
